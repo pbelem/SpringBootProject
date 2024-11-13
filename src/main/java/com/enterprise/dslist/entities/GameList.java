b8701e5096f99e290958c4f4962a7bd602325457
pbelem <pedro.belem@ucsal.edu.br>
2024-11-13 19:55:28 -0300

    Database seed update

--- src/main/java/com/enterprise/dslist/entities/GameList.java
+++ src/main/java/com/enterprise/dslist/entities/GameList.java
@@ -10,21 +10,21 @@ import jakarta.persistence.Table;
 
 
 @Entity
-@Table(name = "tb_list")
+@Table(name = "tb_game_list")
 public class GameList {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String nome;
+	private String name;
 	
 	public GameList () {		
 	}
 
 
-	public GameList(Long id, String nome) {
+	public GameList(Long id, String name) {
 		this.id = id;
-		this.nome = nome;
+		this.name = name;
 	}
 
 
@@ -39,12 +39,12 @@ public class GameList {
 
 
 	public String getNome() {
-		return nome;
+		return name;
 	}
 
 
-	public void setNome(String nome) {
-		this.nome = nome;
+	public void setNome(String name) {
+		this.name = name;
 	}
 
 
